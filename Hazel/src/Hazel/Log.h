7d1a9a2ab96dcdd09493b2cb47e73bd16568253e
Rajan Sapkota <rajaninspirion@gmail.com>
2024-03-11 15:04:40 +0545

    feat: added log file

---
+++ Hazel/src/Hazel/Log.h
@@ -0,0 +1,27 @@
+#include<memory>
+#include "Core.h"
+#include "spdlog/spdlog.h"
+namespace Hazel {
+	class HZ_API Log
+	{
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+
+
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() {
+			return s_CoreLogger;
+		}
+
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() {
+			return s_ClientLogger;
+		}
+
+
+	};
+}
+
+
